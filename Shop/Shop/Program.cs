401b3f5aa42c495ab84253b499f207d06316b50d
Gevorg <gulkanyangk@mail.ru>
2021-11-22 22:02:25 +0300

    fix

--- Shop/Shop/Program.cs
+++ Shop/Shop/Program.cs
@@ -1,22 +1,147 @@
-﻿namespace Shop
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shop
 {
-    Good iPhone12 = new Good("IPhone 12");
-    Good iPhone11 = new Good("IPhone 11");
+    public class Program
+    {
+        private readonly Shop _shop = new Shop(new GoodCellContainer(), new GoodCellContainer());
+        private readonly Good _iPhone12 = new Good("IPhone 12");
+        private readonly Good _iPhone11 = new Good("IPhone 11");
 
-    Warehouse warehouse = new Warehouse();
+        private void Start()
+        {
+            _shop.AddToWarehouse(new GoodCell(_iPhone12, 10), new GoodCell(_iPhone11, 1));
 
-    Shop shop = new Shop(warehouse);
+            foreach (var goodCell in _shop.Warehouse.Goods)
+            {
+                Console.WriteLine($"Товар: {goodCell.Good.Name}, Кол-во: {goodCell.Count}");
+            }
+            ShowGoods(_shop.Warehouse.Goods);
 
-    warehouse.Delive(iPhone12, 10);
-    warehouse.Delive(iPhone11, 1);
+            _shop.AddToCart(new GoodCell(_iPhone12, 4), new GoodCell(_iPhone11, 3));
+            
+            ShowGoods(_shop.Cart.Goods);
+        }
 
-//Вывод всех товаров на складе с их остатком
+        private void ShowGoods(IEnumerable<GoodCell> cells)
+        {
+            foreach (var goodCell in cells)
+            {
+                Console.WriteLine($"Товар: {goodCell.Good.Name}, Кол-во: {goodCell.Count}");
+            }
+        }
+    }
 
-    Cart cart = shop.Cart();
-    cart.Add(iPhone12, 4);
-    cart.Add(iPhone11, 3); //при такой ситуации возникает ошибка так, как нет нужного количества товара на складе
+    public class Shop
+    {
+        public Shop(GoodCellContainer warehouse, GoodCellContainer cart)
+        {
+            Warehouse = warehouse ?? throw new InvalidOperationException();
+            Cart = cart ?? throw new InvalidOperationException();
+        }
 
-//Вывод всех товаров в корзине
+        public GoodCellContainer Warehouse { get; private set; }
+        public GoodCellContainer Cart { get; private set; }
 
-    Console.WriteLine(cart.Order().Paylink);
+        public void AddToWarehouse(params GoodCell[] cells)
+        {
+            foreach (var cell in cells)
+            {
+                Warehouse.Add(cell);
+            }
+        }
+
+        public void AddToCart(params GoodCell[] cells)
+        {
+            foreach (var cell in cells)
+            {
+                try
+                {
+                    Warehouse.Remove(cell);
+                    Cart.Add(cell);
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception);
+                }
+            }
+        }
+    }
+
+    public class GoodCellContainer
+    {
+        private readonly List<GoodCell> _goods = new List<GoodCell>();
+        public IEnumerable<GoodCell> Goods => _goods;
+
+        public void Add(GoodCell goodCell)
+        {
+            GoodCell currentCell = _goods.FirstOrDefault(cell => cell.Good.Name == goodCell.Good.Name);
+
+            if (currentCell.Good == null)
+            {
+                _goods.Add(goodCell);
+            }
+            else
+            {
+                currentCell.Merge(goodCell);
+            }
+        }
+
+        public void Remove(GoodCell goodCell)
+        {
+            GoodCell currentCell = _goods.FirstOrDefault(cell => cell.Good.Name == goodCell.Good.Name);
+
+            if (currentCell.Good == null)
+                throw new Exception("Товара не существует");
+
+            currentCell.Remove(goodCell);
+        }
+    }
+
+    public struct GoodCell
+    {
+        public GoodCell(Good good, int count)
+        {
+            if (count < 0)
+                throw new InvalidOperationException();
+            if (good == null)
+                throw new InvalidOperationException();
+
+            Good = good;
+            Count = count;
+        }
+
+        public Good Good { get; private set; }
+        public int Count { get; private set; }
+
+        public void Merge(GoodCell cell)
+        {
+            if (cell.Good.Name != Good.Name)
+                throw new InvalidOperationException();
+
+            Count += cell.Count;
+        }
+
+        public void Remove(GoodCell cell)
+        {
+            if (cell.Good.Name != Good.Name)
+                throw new InvalidOperationException();
+            if (cell.Count > Count)
+                throw new InvalidOperationException();
+
+            Count -= cell.Count;
+        }
+    }
+
+    public class Good
+    {
+        public Good(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; private set; }
+    }
 }
