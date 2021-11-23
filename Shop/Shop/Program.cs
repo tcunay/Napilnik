f970539c0d45f2e29b16b261848579e84765073a
Gevorg <gulkanyangk@mail.ru>
2021-11-23 20:45:50 +0300

    Update Program.cs

--- Shop/Shop/Program.cs
+++ Shop/Shop/Program.cs
@@ -68,84 +68,85 @@ namespace Shop
                 _cart.Add(cell);
             }
         }
+    }
 
-        public interface IReadonlyCellContainer
+    public interface IReadonlyCellContainer
+    {
+        IEnumerable<GoodCell> Cells { get; }
+    }
+
+    public class GoodCellContainer : IReadonlyCellContainer
+    {
+        private readonly List<GoodCell> _goods = new List<GoodCell>();
+        public IEnumerable<GoodCell> Cells => _goods;
+
+        public void Add(GoodCell goodCell)
         {
-            IEnumerable<GoodCell> Cells { get; }
-        }
+            GoodCell currentCell = _goods.FirstOrDefault(cell => cell.Good.Name == goodCell.Good.Name);
 
-        public class GoodCellContainer : IReadonlyCellContainer
-        {
-            private readonly List<GoodCell> _goods = new List<GoodCell>();
-            public IEnumerable<GoodCell> Cells => _goods;
-
-            public void Add(GoodCell goodCell)
+            if (currentCell.Good == null)
             {
-                GoodCell currentCell = _goods.FirstOrDefault(cell => cell.Good.Name == goodCell.Good.Name);
-
-                if (currentCell.Good == null)
-                {
-                    _goods.Add(goodCell);
-                }
-                else
-                {
-                    currentCell.Merge(goodCell);
-                }
+                _goods.Add(goodCell);
             }
-
-            public void Remove(GoodCell goodCell)
+            else
             {
-                GoodCell currentCell = _goods.FirstOrDefault(cell => cell.Good.Name == goodCell.Good.Name);
-
-                if (currentCell.Good == null)
-                    throw new Exception("Товара не существует");
-
-                currentCell.Remove(goodCell);
+                currentCell.Merge(goodCell);
             }
         }
 
-        public struct GoodCell
+        public void Remove(GoodCell goodCell)
         {
-            public GoodCell(Good good, int count)
-            {
-                if (count < 0)
-                    throw new InvalidOperationException();
-                if (good == null)
-                    throw new InvalidOperationException();
+            GoodCell currentCell = _goods.FirstOrDefault(cell => cell.Good.Name == goodCell.Good.Name);
 
-                Good = good;
-                Count = count;
-            }
+            if (currentCell.Good == null)
+                throw new Exception("Товара не существует");
 
-            public Good Good { get; private set; }
-            public int Count { get; private set; }
-
-            public void Merge(GoodCell cell)
-            {
-                if (cell.Good.Name != Good.Name)
-                    throw new InvalidOperationException();
-
-                Count += cell.Count;
-            }
-
-            public void Remove(GoodCell cell)
-            {
-                if (cell.Good.Name != Good.Name)
-                    throw new InvalidOperationException();
-                if (cell.Count > Count)
-                    throw new InvalidOperationException();
-
-                Count -= cell.Count;
-            }
-        }
-
-        public class Good
-        {
-            public Good(string name)
-            {
-                Name = name;
-            }
-
-            public string Name { get; private set; }
+            currentCell.Remove(goodCell);
         }
     }
+
+    public struct GoodCell
+    {
+        public GoodCell(Good good, int count)
+        {
+            if (count < 0)
+                throw new InvalidOperationException();
+            if (good == null)
+                throw new InvalidOperationException();
+
+            Good = good;
+            Count = count;
+        }
+
+        public Good Good { get; private set; }
+        public int Count { get; private set; }
+
+        public void Merge(GoodCell cell)
+        {
+            if (cell.Good.Name != Good.Name)
+                throw new InvalidOperationException();
+
+            Count += cell.Count;
+        }
+
+        public void Remove(GoodCell cell)
+        {
+            if (cell.Good.Name != Good.Name)
+                throw new InvalidOperationException();
+            if (cell.Count > Count)
+                throw new InvalidOperationException();
+
+            Count -= cell.Count;
+        }
+    }
+
+    public class Good
+    {
+        public Good(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; private set; }
+    }
+}
