275206ff4f8694e2529224313d5063edcee9390e
Gevorg <gulkanyangk@mail.ru>
2021-11-23 19:41:58 +0300

    fix

--- Shop/Shop/Program.cs
+++ Shop/Shop/Program.cs
@@ -13,11 +13,19 @@ namespace Shop
         private void Start()
         {
             _shop.AddToWarehouse(new GoodCell(_iPhone12, 10), new GoodCell(_iPhone11, 1));
-            
+
             ShowGoods(_shop.Warehouse.Cells);
 
-            _shop.AddToCart(new GoodCell(_iPhone12, 4), new GoodCell(_iPhone11, 3));
-            
+            try
+            {
+                _shop.AddToCart(new GoodCell(_iPhone12, 4), new GoodCell(_iPhone11, 3));
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+
+
             ShowGoods(_shop.Cart.Cells);
         }
 
@@ -34,7 +42,7 @@ namespace Shop
     {
         private readonly GoodCellContainer _warehouse;
         private readonly GoodCellContainer _cart;
-        
+
         public Shop(GoodCellContainer warehouse, GoodCellContainer cart)
         {
             _warehouse = warehouse ?? throw new InvalidOperationException();
@@ -56,96 +64,88 @@ namespace Shop
         {
             foreach (var cell in cells)
             {
-                try
-                {
-                    _warehouse.Remove(cell);
-                    _cart.Add(cell);
-                }
-                catch (Exception exception)
-                {
-                    Console.WriteLine(exception);
-                }
+                _warehouse.Remove(cell);
+                _cart.Add(cell);
             }
         }
-    }
 
-    public interface IReadonlyCellContainer
-    {
-        IEnumerable<GoodCell> Cells { get; }
-    }
-
-    public class GoodCellContainer : IReadonlyCellContainer
-    {
-        private readonly List<GoodCell> _goods = new List<GoodCell>();
-        public IEnumerable<GoodCell> Cells => _goods;
-
-        public void Add(GoodCell goodCell)
+        public interface IReadonlyCellContainer
         {
-            GoodCell currentCell = _goods.FirstOrDefault(cell => cell.Good.Name == goodCell.Good.Name);
+            IEnumerable<GoodCell> Cells { get; }
+        }
 
-            if (currentCell.Good == null)
+        public class GoodCellContainer : IReadonlyCellContainer
+        {
+            private readonly List<GoodCell> _goods = new List<GoodCell>();
+            public IEnumerable<GoodCell> Cells => _goods;
+
+            public void Add(GoodCell goodCell)
             {
-                _goods.Add(goodCell);
+                GoodCell currentCell = _goods.FirstOrDefault(cell => cell.Good.Name == goodCell.Good.Name);
+
+                if (currentCell.Good == null)
+                {
+                    _goods.Add(goodCell);
+                }
+                else
+                {
+                    currentCell.Merge(goodCell);
+                }
             }
-            else
+
+            public void Remove(GoodCell goodCell)
             {
-                currentCell.Merge(goodCell);
+                GoodCell currentCell = _goods.FirstOrDefault(cell => cell.Good.Name == goodCell.Good.Name);
+
+                if (currentCell.Good == null)
+                    throw new Exception("Товара не существует");
+
+                currentCell.Remove(goodCell);
             }
         }
 
-        public void Remove(GoodCell goodCell)
+        public struct GoodCell
         {
-            GoodCell currentCell = _goods.FirstOrDefault(cell => cell.Good.Name == goodCell.Good.Name);
+            public GoodCell(Good good, int count)
+            {
+                if (count < 0)
+                    throw new InvalidOperationException();
+                if (good == null)
+                    throw new InvalidOperationException();
 
-            if (currentCell.Good == null)
-                throw new Exception("Товара не существует");
+                Good = good;
+                Count = count;
+            }
 
-            currentCell.Remove(goodCell);
+            public Good Good { get; private set; }
+            public int Count { get; private set; }
+
+            public void Merge(GoodCell cell)
+            {
+                if (cell.Good.Name != Good.Name)
+                    throw new InvalidOperationException();
+
+                Count += cell.Count;
+            }
+
+            public void Remove(GoodCell cell)
+            {
+                if (cell.Good.Name != Good.Name)
+                    throw new InvalidOperationException();
+                if (cell.Count > Count)
+                    throw new InvalidOperationException();
+
+                Count -= cell.Count;
+            }
+        }
+
+        public class Good
+        {
+            public Good(string name)
+            {
+                Name = name;
+            }
+
+            public string Name { get; private set; }
         }
     }
-
-    public struct GoodCell
-    {
-        public GoodCell(Good good, int count)
-        {
-            if (count < 0)
-                throw new InvalidOperationException();
-            if (good == null)
-                throw new InvalidOperationException();
-
-            Good = good;
-            Count = count;
-        }
-
-        public Good Good { get; private set; }
-        public int Count { get; private set; }
-
-        public void Merge(GoodCell cell)
-        {
-            if (cell.Good.Name != Good.Name)
-                throw new InvalidOperationException();
-
-            Count += cell.Count;
-        }
-
-        public void Remove(GoodCell cell)
-        {
-            if (cell.Good.Name != Good.Name)
-                throw new InvalidOperationException();
-            if (cell.Count > Count)
-                throw new InvalidOperationException();
-
-            Count -= cell.Count;
-        }
-    }
-
-    public class Good
-    {
-        public Good(string name)
-        {
-            Name = name;
-        }
-
-        public string Name { get; private set; }
-    }
-}
